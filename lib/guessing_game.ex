72d59a61f5368fe30037c21c06e95e11e69390d9
gerald-annan <geraldannan400@gmail.com>
2021-12-15 21:21:16 +0000

    Solved

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -1,13 +1,13 @@
 defmodule GuessingGame do
+  def compare(secret_number, guess \\ :no_guess)
+      when guess == :no_guess do
+    "Make a guess"
+  end
+
   def compare(secret_number, guess)
       when secret_number == guess do
     # Please implement the compare/2 function
     "Correct"
-  end
-
-  def compare(secret_number, guess \\ :no_guess)
-      when guess == :no_guess do
-    "Make a guess"
   end
 
   def compare(secret_number, guess)
