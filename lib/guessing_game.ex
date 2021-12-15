abffbf09d171ff51a356687319472915d25cbdc8
AnnanG2 <annang2@stanbic.com.gh>
2021-12-15 18:24:51 +0000

    Solved

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -1,5 +1,27 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
+  def compare(secret_number, guess)
+      when secret_number == guess do
     # Please implement the compare/2 function
+    "Correct"
+  end
+
+  def compare(secret_number, guess \\ :no_guess)
+      when guess == :no_guess do
+    "Make a guess"
+  end
+
+  def compare(secret_number, guess)
+      when abs(secret_number - guess) == 1 do
+    "So close"
+  end
+
+  def compare(secret_number, guess)
+      when guess > secret_number do
+    "Too high"
+  end
+
+  def compare(secret_number, guess)
+      when guess < secret_number do
+    "Too low"
   end
 end
